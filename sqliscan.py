a5df9415587c8110ecf6e2e12eb4371d3d43eb62
anthony <45970315+catakloon@users.noreply.github.com>
2023-09-09 17:10:10 -0400

    update

--- sqliscan.py
+++ sqliscan.py
@@ -3,7 +3,7 @@ from bs4 import BeautifulSoup
 import sys
 from urllib.parse import urljoin
 
-s = requests.Sessions()
+s = requests.Session()
 s.headers['User-agent'] = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/117.0'
 
 # function to get all html forms
